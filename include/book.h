1eb04ebf8abb67717fad1c349b1cb9c9dc184705
devanshwalia <wdevansh02@gmail.com>
2025-12-03 01:55:45 +0530

    Add files via upload

---
+++ include/book.h
@@ -0,0 +1,24 @@
+#ifndef BOOK_H
+#define BOOK_H
+
+#define TITLE_LEN 100
+#define AUTHOR_LEN 100
+
+typedef struct {
+    int id;
+    char title[TITLE_LEN];
+    char author[AUTHOR_LEN];
+    int copies;   // total copies
+    int issued;   // copies issued
+} Book;
+
+// Core operations
+void add_book();
+void display_books();
+void search_book_by_id();
+void search_book_by_title();
+void issue_book();
+void return_book();
+void delete_book();
+
+#endif // BOOK_H
